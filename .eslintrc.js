e71b64ed5fb80536a59a0bf6e90d14d6107a5dec
Johny Herangi <johnyherangi@gmail.com>
2021-10-17 10:35:40 +1300

    use jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,32 @@
 module.exports = {
-    parser: "@typescript-eslint/parser",
-    extends: [
-        "plugin:@typescript-eslint/recommended",
-        "prettier/@typescript-eslint",
-        "plugin:prettier/recommended",
-    ],
-    parserOptions: {
-        ecmaVersion: 2018,
-        sourceType: "module",
+    extends: ["eslint:recommended", "plugin:prettier/recommended"],
+    globals: {
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly",
     },
+    overrides: [
+        {
+            files: ["*.ts"],
+            plugins: ["@typescript-eslint", "fp-ts"],
+            extends: [
+                "plugin:@typescript-eslint/eslint-recommended",
+                "plugin:@typescript-eslint/recommended",
+                "plugin:fp-ts/recommended",
+                "prettier/@typescript-eslint",
+            ],
+            rules: {
+                "@typescript-eslint/no-unused-vars": [
+                    "warn",
+                    {
+                        argsIgnorePattern: "^_",
+                    },
+                ],
+            },
+        },
+        {
+            files: ["*.test.ts"],
+            plugins: ["jest"],
+            rules: {},
+        },
+    ],
 }
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# node-cli-template
+# axios-io-ts
 
 ## Actions
 
