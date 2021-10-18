8a0d5d612d66f6264fb3eefc801d30a81eccd054
Johny Herangi <johnyherangi@gmail.com>
2021-10-18 22:32:24 +1300

    Accept decoder as t.Mixed type (#4)
    * accept decoder as t.Mixed
    
    * bump minor version
    
    * update package-lock

--- src/request.ts
+++ src/request.ts
@@ -1,61 +1,64 @@
 import axios, { AxiosResponse } from "axios"
+import * as t from "io-ts"
 import { RequestConfig } from "src"
 import { decode } from "./decode"
 
-export const httpRequest = async <T, D>(
+export const httpRequest = async <T extends t.Mixed, D = unknown>(
     config: RequestConfig<T, D>,
-): Promise<AxiosResponse<T, D>> => {
+): Promise<AxiosResponse<t.TypeOf<T>, D>> => {
     return await axios.request(config).then((response) => {
         if (config.decoder) {
             decode(response.data, config.url, config.decoder)
         }
-        return response as AxiosResponse<T, D>
+        return response as AxiosResponse<t.TypeOf<T>, D>
     })
 }
 
-export const httpDelete = async <T = unknown, D = unknown>(
+export const httpDelete = async <T extends t.Mixed, D = unknown>(
     config: RequestConfig<T, D>,
-): Promise<AxiosResponse<T, D>> =>
+): Promise<AxiosResponse<t.TypeOf<T>, D>> =>
     httpRequest({
         ...config,
         method: "DELETE",
     })
 
-export const httpGet = async <T = unknown, D = unknown>(
+httpDelete({ url: "test" }).then()
+
+export const httpGet = async <T extends t.Mixed, D = unknown>(
     config: RequestConfig<T, D>,
-): Promise<AxiosResponse<T, D>> =>
+): Promise<AxiosResponse<t.TypeOf<T>, D>> =>
     httpRequest({
         ...config,
         method: "GET",
     })
 
-export const httpOptions = async <T, D>(
+export const httpOptions = async <T extends t.Mixed, D>(
     config: RequestConfig<T, D>,
-): Promise<AxiosResponse<T, D>> =>
+): Promise<AxiosResponse<t.TypeOf<T>, D>> =>
     httpRequest({
         ...config,
         method: "OPTIONS",
     })
 
-export const httpPatch = async <T = unknown, D = unknown>(
+export const httpPatch = async <T extends t.Mixed, D = unknown>(
     config: RequestConfig<T, D>,
-): Promise<AxiosResponse<T, D>> =>
+): Promise<AxiosResponse<t.TypeOf<T>, D>> =>
     httpRequest({
         ...config,
         method: "PATCH",
     })
 
-export const httpPost = async <T = unknown, D = unknown>(
+export const httpPost = async <T extends t.Mixed, D = unknown>(
     config: RequestConfig<T, D>,
-): Promise<AxiosResponse<T, D>> =>
+): Promise<AxiosResponse<t.TypeOf<T>, D>> =>
     httpRequest({
         ...config,
         method: "POST",
     })
 
-export const httpPut = async <T = unknown, D = unknown>(
+export const httpPut = async <T extends t.Mixed, D = unknown>(
     config: RequestConfig<T, D>,
-): Promise<AxiosResponse<T, D>> =>
+): Promise<AxiosResponse<t.TypeOf<T>, D>> =>
     httpRequest({
         ...config,
         method: "PUT",
